f58a4531636f18eb7c753bceafffbe875152010d
William <cabiam@criptext.com>
2024-04-24 13:02:24 +0200

    feat: added python script to encode_anon_secret.py from a given jwt key

---
+++ docker/encode_anon_secret.py
@@ -0,0 +1,23 @@
+# from jwt import jwt
+import jwt
+# import requests
+
+# Define your secret key
+secret_key = "jpoaisjfdpijinzklxcpoiwPDJFAPSIiohsadfphaspdfj2342"  # Replace with your actual secret key
+
+payload_roles = ["anon", "service_role"]
+
+
+
+for payload_role in payload_roles:
+  # Define your payload
+  payload = {
+      "role": payload_role,
+      "iat": 1625137684,  # Issued At Time
+      "exp": 2013955532909   # Expiry Time
+  } 
+
+  # Generate the JWT token
+  jwt_token = jwt.encode(payload, secret_key, algorithm='HS256')
+  print("%s: %s" % (payload_role.upper(), jwt_token.upper()))
+  # print(jwt_token.decode('utf-8'))
